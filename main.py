f33ebed06667d0661e62c459baae246f86d3b2d8
skwon2345 <skwon2345@gmail.com>
2020-10-19 18:40:51 +0900

    first'

---
+++ main.py
@@ -0,0 +1,52 @@
+#flask
+from flask_cors import CORS
+from flask import Flask, render_template, jsonify, request
+
+#database
+import db as conn
+
+app = Flask(__name__)
+
+# Settings
+CORS(app)
+
+@app.route('/')
+def hello():
+    try:
+        doc_ref_overall = conn.db.collection(u'test11').document('aaa')
+        doc_ref_overall.set({
+           'name':'los angefles'
+        })
+
+        return jsonify({"success":True}), 200
+    except Exception as e:
+        print("fesf")
+        print(e)
+        return f"An Error Occured: {e}"
+    return 'Hello World!'
+
+@app.route('/users', methods=['POST'])
+def createUser():
+    print(request.json)
+    try:
+        doc_ref_overall = conn.db.collection(u'test11').document('fe')
+        doc_ref_overall.set(request.json)
+        print("efe")
+
+        return jsonify({"success":True}), 200
+    except Exception as e:
+        print("fesf")
+        print(e)
+        return f"An Error Occured: {e}"
+        
+
+@app.route('/users', methods=['GET'])
+def getUsers():
+    try:
+        todo = conn.db.collection(u'test11').document('fe').get()
+        return jsonify(todo.to_dict()), 200
+    except Exception as e:
+        return f"An Error Occured; {e}"
+
+if __name__=='__main__':
+    app.run(host='127.0.0.1', port=8088, debug=True)
