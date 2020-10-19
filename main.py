6978f8c85f7fa2587de01bd71b889dae51cdd9cb
skwon2345 <skwon2345@gmail.com>
2020-10-19 17:16:17 +0900

    secured

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+#flask
 from flask_cors import CORS
 from flask import Flask, render_template, jsonify, request
-#database 
+
 #database 
 import firebase_admin
 from firebase_admin import credentials, firestore, initialize_app
 
-cred = credentials.Certificate('key.json')
+#dotenv
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+key = os.environ.get("DB_KEY")
+
+cred = credentials.Certificate(key)
 default_app = initialize_app(cred)
 db = firestore.client()
 
