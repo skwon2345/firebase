7fe8978fa0709f8200c8fd12acd2325b742455b8
skwon2345 <skwon2345@gmail.com>
2020-10-27 23:19:25 +0900

    d

--- main.py
+++ main.py
@@ -96,6 +96,6 @@ def getStorageFile():
         return f"An Error occured: {e}"
 
 if __name__=='__main__':
-    app.run(host='127.0.0.1', port=8088, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
 
 
