82a981c0e5aa9cf94d84f59c48d509070a220433
skwon2345 <skwon2345@gmail.com>
2020-10-30 15:34:32 +0900

    e

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import db as conn
 from firebase_admin import firestore
 
 #stock data
-import FinanceDataReader as fdr
-import numpy as np
+# import FinanceDataReader as fdr
+# import numpy as np
 
-import time
+# import time
 
 app = Flask(__name__)
 
@@ -18,8 +18,8 @@ app = Flask(__name__)
 CORS(app)
 
 
-tickers = fdr.StockListing('KOSPI')['Symbol'].values
-stckListing = fdr.StockListing('KOSPI')
+# tickers = fdr.StockListing('KOSPI')['Symbol'].values
+# stckListing = fdr.StockListing('KOSPI')
 
 
 def calcSMA (values, window):
@@ -80,43 +80,45 @@ def getStorageFile():
 @app.route('/api/data',methods=['GET'])
 def testStockAPI():
     dataToSend = []
-    start = time.time()
-    for ticker in tickers:
-        print(ticker)
-        try:
-            if len(ticker) <= 6:
-                df_reverse = fdr.DataReader(ticker)
-                df = df_reverse.iloc[::-1]
-                if len(df) < 200:
-                        continue
-            else:
-                continue
-##  df, meta_data = ts.get_intraday(symbol=ticker,interval='60min', outputsize='full')
-        except Exception as e:
-            return f"An Error occured: {e}"
-
-        selectedStck = stckListing.loc[stckListing['Symbol']==str(ticker)]
-        stckMarket = str(selectedStck['Market'].values[0])
-
-        if stckMarket == "KONEX":
-                continue
-        sma_df = df['Close']
-        sma_5 = calcSMA(sma_df, 5)
-        sma_10 = calcSMA(sma_df, 10)
-        sma_20 = calcSMA(sma_df, 20)
-        sma_60 = calcSMA(sma_df, 60)
-        sma_120 = calcSMA(sma_df, 120)
-        sma_200 = calcSMA(sma_df, 200)
-        if sma_5[1] < sma_10[1] and sma_5[0] > sma_10[0]:
-            dataToSend.append(ticker)
-
-    a = df.head(5).to_dict('index')
-    a = {str(k)[:10]:v for k,v in a.items()}
-    timeTaken = time.time() - start
-    dataToSend.insert(0, str(timeTaken))
-    print(dataToSend)
-    print(timeTaken)
     return jsonify(dataToSend), 200
+
+#     start = time.time()
+#     for ticker in tickers:
+#         print(ticker)
+#         try:
+#             if len(ticker) <= 6:
+#                 df_reverse = fdr.DataReader(ticker)
+#                 df = df_reverse.iloc[::-1]
+#                 if len(df) < 200:
+#                         continue
+#             else:
+#                 continue
+# ##  df, meta_data = ts.get_intraday(symbol=ticker,interval='60min', outputsize='full')
+#         except Exception as e:
+#             return f"An Error occured: {e}"
+
+#         selectedStck = stckListing.loc[stckListing['Symbol']==str(ticker)]
+#         stckMarket = str(selectedStck['Market'].values[0])
+
+#         if stckMarket == "KONEX":
+#                 continue
+#         sma_df = df['Close']
+#         sma_5 = calcSMA(sma_df, 5)
+#         sma_10 = calcSMA(sma_df, 10)
+#         sma_20 = calcSMA(sma_df, 20)
+#         sma_60 = calcSMA(sma_df, 60)
+#         sma_120 = calcSMA(sma_df, 120)
+#         sma_200 = calcSMA(sma_df, 200)
+#         if sma_5[1] < sma_10[1] and sma_5[0] > sma_10[0]:
+#             dataToSend.append(ticker)
+
+#     a = df.head(5).to_dict('index')
+#     a = {str(k)[:10]:v for k,v in a.items()}
+#     timeTaken = time.time() - start
+#     dataToSend.insert(0, str(timeTaken))
+#     print(dataToSend)
+#     print(timeTaken)
+#     return jsonify(dataToSend), 200
 
     
 
