5ea270ea6d9f8666c3791d8efbd30dc142839482
skwon2345 <skwon2345@gmail.com>
2020-11-01 23:18:48 +0900

    e

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def sendEmail(name, emailAddress, content):
     # 제목
     subject = 'Contact from ' + name 
 
-    msg = MIMEMultipart('alternative')
+    msg = MIMEMultipart('mixed')
     msg['From'] = email_user
     msg['To'] = email_send
     msg['Subject'] = subject
