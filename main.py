7784b41204146f68860ba72358490d09ba71efb2
skwon2345 <skwon2345@gmail.com>
2020-11-15 23:00:48 +0900

    e

--- main.py
+++ main.py
@@ -191,8 +191,9 @@ def uploadImageBASE64():
         # model = joblib.load('./models/keras_model_joblib') # model trained with kears.dataset
         ans = model.predict([result])
         print(model.predict([result]))
-
-        return jsonify({"success":int(ans[0])}), 200
+        response = jsonify({"success":int(ans[0])})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response, 200
 
     except Exception as e:
         print(e)
